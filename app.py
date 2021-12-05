9cfab803d4bf00d25ec096677c2be545043dd614
Titilayo Amuwo <teksmi54@outlook.com>
2021-12-05 16:35:48 -0500

    made changes to the files

--- app.py
+++ app.py
@@ -5,24 +5,42 @@ import sklearn
 from PIL import Image
 
 
+html_temp = """
+<div style = "background-color:yellow;padding:13px">
+<h1 style="color:black;text-align:center;">Soccer Match Fixture Betting App</h1>
+</div>
+"""
+
+st.markdown(html_temp, unsafe_allow_html=True)
+
+
 st.image("https://www.pinnacle.com/Cms_Data/Contents/Guest/Media/betting-articles/soccer/strategy/ultimate-guide-soccer-hero.jpg")
 
-st.title("Soccer Match Fixture Betting App")
 
 st.header("This App predict Soccer match fixture using the Soccer Power Index and the Probabilities of winning at Home or Away.")
 
 st.markdown("""<a href="https://projects.fivethirtyeight.com/global-club-soccer-rankings/">Real-time SPI Rankings</a>""",
             unsafe_allow_html=True)
 
+html= """
+<div style = "background-color:yellow;padding:8px">
+<h1 style="color:black;text-align:center;">Club Data</h1>
+</div>
+"""
+
+st.markdown(html,unsafe_allow_html=True)
+
+df= pd.read_csv("Data/soccer-spi/club_data.csv",index_col=0)
+
+st.write(df)
+
 st.subheader("Input match data below:")
 
-
-
 spi1 = st.slider("What is the home team's ESPN SPI rating?", 
-                 min_value=1, max_value=100)
+                 min_value=1.00, max_value=99.00)
 
 spi2 = st.slider("What is the away team's ESPN SPI rating?",
-                 min_value=1, max_value=100)
+                 min_value=1.00, max_value=99.00)
 
 prob1 = st.slider("What are the bookmaker odds on the home team winning?",
                  min_value=0.001, max_value=1.000)
@@ -32,34 +50,44 @@ prob2 = st.slider("What are the bookmaker odds on the away team winning?",
 
 probtie = round((1.000 - (prob1 + prob2)), 3)
 
+season = st.slider("What season is the match",
+                 min_value=2016, max_value=2022)
+
+xg1 = st.slider("What is the expected goals on the Home team?",
+                 min_value=0.01, max_value=5.00)
+
+xg2 = st.slider("What is the expected goals on the Away team?",
+                 min_value=0.01, max_value=5.00)
+
+importance1 = st.slider("How important is the match to the Home team?",
+                 min_value=1.00, max_value=99.00)
+
+importance2 = st.slider("How important is the match to the Away team?",
+                 min_value=1.00, max_value=99.00)
+
 if st.button("Click here to run!"):
-    st.subheader("Modeling match results off these SPIs and odds:")
-    st.write(f"Home SPI: {spi1}")
-    st.write(f"Away SPI: {spi2}")
+    st.subheader("Modeling match results off these features:")
+    st.write(f"Home team SPI: {spi1}")
+    st.write(f"Away team SPI: {spi2}")
     st.write(f"Bookmaker odds for home win: {prob1}")
     st.write(f"Bookmaker odds for away win: {prob2}")
     st.write(f"Bookmaker odds for tie: {probtie}")
+    st.write(f"Season: {season}")
+    st.write(f"prob of expected goals Home team: {xg1}")
+    st.write(f"prob of expected goals Away team: {xg2}")
+    st.write(f"importance of match to Home team: {importance1}")
+    st.write(f"importance of match to Away team: {importance2}")
     
-    model = pickle.load(open('model.sav','rb'))
-    
-    data = pd.DataFrame(columns = ['season','spi1','spi2','prob1','prob2','probtie','xg1','xg2','importance1','importance2'])
-    data = data.append({'spi1': spi1,
-                        'spi2': spi2,
-                        'prob1': prob1,
-                        'prob2': prob2,
-                        'probtie': probtie,
-                        'season': season,
-                        'xg1':xg1,
-                        'xg2':xg2,
-                        'importance1':importance1,
-                        'importance2':importance2},
-                       ignore_index=True)
-    pred = model.predict(data)
+    deploy_model = pickle.load(open('model.sav','rb'))
+
+
+    pred = deploy_model.predict([[season, spi1, spi2, prob1, prob2, probtie, importance1,
+       importance2, xg1, xg2]])
     
     if pred[0] == 0:
-        st.subheader("Predicted match result: Draw")
+        st.success("The predicted match result is a: Draw")
     elif pred[0] == 1:
-            st.subheader("Predicted match result: Home Win")
+        st.success("The predicted match result is : Home Win")
     else:
-        st.subheader("Predicted match result: Away Win")
+        st.success("The predicted match result is : Away Win")
             
